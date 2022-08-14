2e40bc7ac892fc850b811cd2cfc741cc23d6c9dc
dimitraye <dimitraye94@gmail.com>
2022-08-14 12:18:00 +0200

    step2 / pas finie

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 //const stuffRoutes = require('./routes/stuff');
 
-//const userRoutes = require('./routes/user');
+const userRoutes = require('./routes/user');
 
 //const path = require('path');
 
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 //app.use('/api/stuff', stuffRoutes);
 
-//app.use('/api/auth', userRoutes);
+app.use('/api/auth', userRoutes);
 
 //app.use('/images', express.static(path.join(__dirname, 'images')));
 
